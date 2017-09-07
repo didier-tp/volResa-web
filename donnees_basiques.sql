da71ce938582a0a3c6b50617b458239daed0681e
LeroyJeremy <jrmleroy@wanadoo.fr>
2017-09-07 14:39:26 +0200

    jerem2

---
+++ donnees_basiques.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO client(client, id, c_rue, c_cp, c_pays, c_ville, email, nom, prenom, titre, siret, version) 
+   VALUES ('physique', 1, 'rue rougement', '75009', 'france', 'paris', 'oliviergazlan@ajcformation.com', 'gazlan', 'olivier', null, null, 0),
+	('moral', 2, '6 place iris', '92400', 'france', 'courbevoie', 'sopra@soprasteria.com', 'SopraSteria', null, 'SAS', '06548698958', 0);
+
+INSERT INTO passager(id, p_rue, p_cp, p_pays, p_ville, nom, prenom, version) 
+   VALUES (1, 'rue de etang', '78480', 'france', 'vernuil', 'leroy', 'jeremy', 0),
+	(2, 'elysee', '75000', 'france', 'paris', 'macron', 'emmanuel', 0),
+	(3, 'rue rougement', '75009', 'france', 'paris', 'gazlan', 'olivier', 0);
+   
+INSERT INTO reservations(id, numero, client_id, passager_id, version) 
+   VALUES ( 1, 256, 2, 1, 0),
+	( 2, 342, 2, 2, 0),
+	( 3, 1002, 1, 3, 0);
+   
--- src/main/webapp/WEB-INF/pages/reservation/list.jsp
+++ src/main/webapp/WEB-INF/pages/reservation/list.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -9,6 +10,25 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>Liste des reservations</h1>
+<table border="1">
+<tr>
+<th>id</th>
+<th>date</th>
+<th>numero</th>
+<th>passager</th>
+<th>vol</th>
+</tr>
+<c:forEach items="${liste }" var="p">
+<tr>
+<td>${p.id }</td>
+<td>${p.date }</td>
+<td>${p.numero }</td>
+<td>${p.passager.nom }</td>
+<td>${p.vol.id }</td>
+</tr>
+</c:forEach>
+</table>
 
 </body>
 </html>
