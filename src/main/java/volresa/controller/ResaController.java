5df15bd5bf06cc1c1713f8ae30deafff079af90b
LeroyJeremy <jrmleroy@wanadoo.fr>
2017-09-07 15:31:39 +0200

    crud

--- src/main/java/volresa/controller/ResaController.java
+++ src/main/java/volresa/controller/ResaController.java
@@ -1,11 +1,19 @@
 package volresa.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import springMVC2.controller.Eleve;
+import springMVC2.controller.Formateur;
+import springMVC2.controller.Personne;
 import vol.metier.dao.ReservationDao;
+import vol.metier.model.Reservation;
 
 @Controller
 @RequestMapping("/reservation")
@@ -20,5 +28,30 @@ public class ResaController {
 		return new ModelAndView("/reservation/list", "liste", resaDao.findAll());
 	}
 	
+	@RequestMapping("/delete")
+	public ModelAndView delete(@RequestParam(name="id", required=true) Long id) {
+		resaDao.delete(resaDao.find(id));
+		return new ModelAndView("redirect:list");
+	}
 	
+	
+	@RequestMapping("/edit")
+	public ModelAndView edit(@RequestParam(name="id", required=false) Long id) {
+		if (id == null) {
+			return new ModelAndView("reservation/edit", "uneResa", new Reservation());
+		}else {
+			return new ModelAndView("reservation/edit", "uneResa", resaDao.find(id));
+		}
+	}
+	
+	@RequestMapping("/save")
+	public ModelAndView save(@ModelAttribute("uneResa") @Valid Reservation r) {
+		if (r.getId()==null) {
+			resaDao.create(r);
+		}else {
+			resaDao.update(r);
+		}
+		return new ModelAndView("redirect:list");
+	}
+		
 }
