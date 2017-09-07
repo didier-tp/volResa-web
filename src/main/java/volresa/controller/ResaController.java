bbed0235cc2bd656841b1bd138cf76e57e4c6cbd
dedeledindon <exemple@gmail.com>
2017-09-07 14:53:43 +0200

    v1.0

---
+++ src/main/java/volresa/controller/ResaController.java
@@ -0,0 +1,24 @@
+package volresa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import vol.metier.dao.ReservationDao;
+
+@Controller
+@RequestMapping("/reservation")
+public class ResaController {
+
+	@Autowired
+	private ReservationDao resaDao;
+
+	
+	@RequestMapping("/list")
+	public ModelAndView list() {
+		return new ModelAndView("/reservation/list", "liste", resaDao.findAll());
+	}
+	
+	
+}
